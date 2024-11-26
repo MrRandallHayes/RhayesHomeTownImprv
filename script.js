511eee5b7935157ddfef64923d37b9db71b3b7d4
MrRandallHayes <randallhayes90@gmail.com>
2024-11-26 22:59:22 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var carousel;
+
+
+carousel = ['https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/7d/82/41.jpg', 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/0a/06/1d/56.jpg', 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0a/06/1d/63.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLxYqaxI9krMYxNtrI3cKF3nzDVDQ9Exaizg&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDKhv5yKOR2mKJL3N-Taf3xACTChVl7B2xmw&s'];
+let element_image_display = document.getElementById('image-display');
+element_image_display.setAttribute("src", carousel[0]);
+
+
+document.getElementById('prev-btn').addEventListener('click', (event) => {
+  let element_image_display2 = document.getElementById('image-display');
+  carousel.unshift(carousel.pop());
+  element_image_display2.setAttribute("src", carousel.slice(-1)[0]);
+
+});
+
+document.getElementById('next-btn').addEventListener('click', (event) => {
+  let element_image_display3 = document.getElementById('image-display');
+  carousel.push(carousel.shift());
+  element_image_display3.setAttribute("src", carousel[0]);
+
+});
 
